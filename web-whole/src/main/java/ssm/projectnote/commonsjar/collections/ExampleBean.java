be072c6bc633870856c1c1669cfc2a8375cf6a9f
CodingSoldier <tfz9011@163.com>
2017-12-22 23:26:05 +0800

    commons collection4

---
+++ web-whole/src/main/java/ssm/projectnote/commonsjar/collections/ExampleBean.java
@@ -0,0 +1,18 @@
+package ssm.projectnote.commonsjar.collections;
+
+/**
+ * @Author：陈丕迁
+ * @Description：
+ * @Date： 2017/12/22
+ */
+public class ExampleBean {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
