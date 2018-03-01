9d63ed8a5b95316dc540c9ddb854ee80aa140554
CodingSoldier <tfz9011@163.com>
2018-03-01 21:48:04 +0800

    quartz

---
+++ web-whole/src/main/java/ssm/quartz/QuartzSpring.java
@@ -0,0 +1,17 @@
+package ssm.quartz;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author：陈丕迁
+ * @Description：
+ * @Date： 2018/2/28
+ */
+@Component
+public class QuartzSpring {
+
+    public void say(){
+        System.out.println("**********调度**********");
+    }
+
+}
