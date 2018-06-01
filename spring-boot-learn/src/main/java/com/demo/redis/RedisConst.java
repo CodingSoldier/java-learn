a4652c35cb501b85a426534266cd3c24b785e1c1
CodingSoldier <tfz9011@163.com>
2018-06-01 23:23:12 +0800

    请求请求去去去

--- spring-boot-learn/src/main/java/com/demo/redis/RedisConst.java
+++ spring-boot-learn/src/main/java/com/demo/redis/RedisConst.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Component;
 public class RedisConst {
 
     public static final String SPRING_CACHE = "SPRING:CACHE:";
-    public static final String SPRING_CACHE_ID = "213142sfgdgbdfg";
 
 }
