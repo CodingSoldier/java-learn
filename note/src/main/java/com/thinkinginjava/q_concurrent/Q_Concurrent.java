fdb9619e217f1436c88de2b175550eaf5b5cb137
CodingSoldier <tfz9011@163.com>
2018-06-10 22:07:00 +0800

    ff

--- note/src/main/java/com/thinkinginjava/q_concurrent/Q_Concurrent.java
+++ note/src/main/java/com/thinkinginjava/q_concurrent/Q_Concurrent.java
@@ -2274,6 +2274,54 @@ class WaxOMatic {
 }
 
 
+
+
+
+
+//P706
+class Coop1 implements Runnable {
+    public Coop1() { print("Constructed Coop1"); }
+    public void run() {
+        print("Coop1 going into wait");
+        synchronized(this) {
+            try {
+                wait();
+            } catch(InterruptedException ignore) {}
+        }
+        print("Coop1 exited wait");
+    }
+}
+class Coop2 implements Runnable {
+    Runnable otherTask;
+    public Coop2(Runnable otherTask) {
+        this.otherTask = otherTask;
+        print("Constructed Coop2");
+    }
+    public void run() {
+        print("Coop2 pausing 5 seconds");
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch(InterruptedException ignore) {}
+        print("Coop2 calling notifyAll");
+        synchronized(otherTask) { otherTask.notifyAll(); }
+    }
+}
+class E21_ThreadCooperation {
+    public static void main(String args[]) throws Exception {
+        Runnable coop1 = new Coop1(),
+                coop2 = new Coop2(coop1);
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.execute(coop1);
+        exec.execute(coop2);
+        Thread.yield();
+        exec.shutdown();
+    }
+}
+
+
+
+
+
 //P706
 class E22_BusyWait {
     private static volatile boolean flag;
