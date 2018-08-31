ab51c4601754c4759f1d051f1af88658366a639a
chenpiqian <237053176@qq.com>
2018-08-31 21:03:21 +0800

    00

---
+++ jvm/src/main/java/com/I_I.java
@@ -0,0 +1,10 @@
+package com;
+
+public class I_I {
+    static int a;  //成员变量会赋初值
+    public static void main(String[] args) throws Exception{
+        int b;   //局部变量不会赋初值
+        System.out.println(a);
+        //System.out.println(b);   //编译失败
+    }
+}
