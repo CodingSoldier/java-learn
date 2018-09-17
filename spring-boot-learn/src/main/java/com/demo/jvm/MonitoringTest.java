45d38c1611f0bcf56c10b556de977210a1cf368d
CodingSoldier <tfz9011@163.com>
2018-09-18 00:03:58 +0800

    00

--- spring-boot-learn/src/main/java/com/demo/jvm/MonitoringTest.java
+++ spring-boot-learn/src/main/java/com/demo/jvm/MonitoringTest.java
@@ -1,5 +1,7 @@
 package com.demo.jvm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/jvm")
 public class MonitoringTest {
+    Logger logger = LoggerFactory.getLogger(MonitoringTest.class);
 
     static class OOMObject{
         public byte[] placeholder = new byte[64 * 1024];
