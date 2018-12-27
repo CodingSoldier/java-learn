aaa24b3099205c5f568dc8a2936e98ade7ad5b5c
CodingSoldier <tfz9011@163.com>
2018-12-27 23:51:14 +0800

    00

--- spring-cloud-learn/eureka-consumer-feign/src/main/java/com/cpq/eurekaconsumerfeign/EurekaConsumerFeignApplication.java
+++ spring-cloud-learn/eureka-consumer-feign/src/main/java/com/cpq/eurekaconsumerfeign/EurekaConsumerFeignApplication.java
@@ -1,26 +1,14 @@
 package com.cpq.eurekaconsumerfeign;
 
-import com.cpq.eurekaconsumerfeign.service.MyMessageConverter;
-import feign.codec.Decoder;
-import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.feign.support.SpringDecoder;
-import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaConsumerFeignApplication {
-    @Bean
-    public Decoder feignDecoder(){
-        MyMessageConverter wxConverter = new MyMessageConverter();
-        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(wxConverter);
-        return new SpringDecoder(objectFactory);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaConsumerFeignApplication.class, args);
--- spring-cloud-learn/eureka-consumer-feign/src/main/java/com/cpq/eurekaconsumerfeign/service/SysUserService.java
+++
@@ -1,9 +0,0 @@
-package com.cpq.eurekaconsumerfeign.service;
-
-import com.alibaba.fastjson.JSONObject;
-
-public interface SysUserService{
-
-    JSONObject add();
-
-}
