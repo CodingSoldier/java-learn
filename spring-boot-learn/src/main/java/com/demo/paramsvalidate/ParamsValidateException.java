b65fd3e68cfe65cda236a9358edb60b90e6f455e
CodingSoldier <tfz9011@163.com>
2019-01-13 20:37:06 +0800

    00

--- spring-boot-learn/src/main/java/com/demo/paramsvalidate/ParamsValidateException.java
+++
@@ -1,9 +0,0 @@
-package com.demo.paramsvalidate;
-
-public class ParamsValidateException extends RuntimeException {
-
-    public ParamsValidateException(String message) {
-        super(message);
-    }
-
-}
--- spring-boot-learn/src/main/java/com/demo/paramsvalidate/bean/PvLevel.java
+++
@@ -1,9 +0,0 @@
-package com.demo.paramsvalidate.bean;
-
-public class PvLevel {
-
-    private PvLevel() {}
-
-    public static final String STRICT = "STRICT";  //严格模式，发生异常，校验不通过，默认
-    public static final String LOOSE = "LOOSE";   //宽松模式，发生异常，不校验
-}
