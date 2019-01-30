9239ff6d05ab1b39e7e597d3b790161ee56a642c
CodingSoldier <tfz9011@163.com>
2019-01-30 15:54:46 +0800

    00

--- project/mysql-learn/mysql主从配置2.sql
+++ project/mysql-learn/mysql主从配置2.sql
@@ -36,8 +36,44 @@ show master status;
 change master to master_host='192.168.40.129',master_user='user01',master_password='user01password',master_log_file='mysql-log-bin.000001',master_log_pos=0;
 
 
-#跳过所有错误。复制出错后，复制会停止，在备库配置
+
+# 主库自定义配置
+#不使用密码强度规则
+validate-password=0
+# 主库开启日志功能、设置server_id
+log_bin=/var/lib/mysql/mysql-log-bin
+server_id=10
+#日志同步到磁盘
+sync_binlog=1
+#一个日志最大容量
+max_binlog_size = 500M
+#日志过期时间
+expire_logs_days = 9
+
+
+
+#备库自定义配置
+#不使用密码强度规则
+validate-password=0
+#从数据库
+log_bin=/var/lib/mysql/mysql-log-bin
+server_id=2
+relay_log=/var/lib/mysql/mysql-relay-bin
+#在复制过程中记录自己的二进制日志，备库也可以变成主库
+log_slave_updates=1
+#备库重启后不启动复制，需要手动开启
+#skip_slave_start
+read_only #不常用
+sync_master_info=1
+sync_relay_log=1
+sync_relay_log_info=1
+#复制出错后，跳过所有错误
 slave-skip-errors=all
+#一个日志最大容量
+max_binlog_size = 500M
+#日志过期时间
+expire_logs_days = 9
+
 
 
 
