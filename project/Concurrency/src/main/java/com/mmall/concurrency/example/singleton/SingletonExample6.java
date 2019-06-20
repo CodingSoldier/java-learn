4d4fd84546261a069ae9e3ede82f151770eaaa02
chenpiqian <chenpiqian@shenzhenpoly.com>
2019-06-20 16:28:48 +0800

    00

---
+++ project/Concurrency/src/main/java/com/mmall/concurrency/example/singleton/SingletonExample6.java
@@ -0,0 +1,37 @@
+package com.mmall.concurrency.example.singleton;
+
+import com.mmall.concurrency.annoations.ThreadSafe;
+
+/**
+ * 饿汉模式
+ * 单例实例在类装载时进行创建
+ */
+@ThreadSafe
+public class SingletonExample6 {
+
+    // 私有构造函数
+    private SingletonExample6() {
+
+    }
+
+    static {
+        instance = new SingletonExample6();
+    }
+
+    // 单例对象，有bug
+    //private static SingletonExample6 instance = null;
+    // 单例对象，无bug
+    private static SingletonExample6 instance;
+
+    // 静态的工厂方法
+    public static SingletonExample6 getInstance() {
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getInstance().hashCode());
+        System.out.println(getInstance().hashCode());
+    }
+
+
+}
---
+++ project/Concurrency/src/main/java/com/mmall/concurrency/example/syncContainer/VectorExample2.java
@@ -0,0 +1,36 @@
+package com.mmall.concurrency.example.syncContainer;
+
+import com.mmall.concurrency.annoations.NotThreadSafe;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@NotThreadSafe
+public class VectorExample2 {
+
+    private static List<Integer> vector = new CopyOnWriteArrayList<>();
+
+    public static void main(String[] args) {
+        while (true) {
+            for (int i = 0; i < 10; i++) {
+                vector.add(i);
+            }
+            Thread thread1 = new Thread() {
+                public void run() {
+                    for (int i = 0; i < vector.size(); i++) {
+                        vector.remove(i);
+                    }
+                }
+            };
+            Thread thread2 = new Thread() {
+                public void run() {
+                    for (int i = 0; i < vector.size(); i++) {
+                        vector.get(i);
+                    }
+                }
+            };
+            thread1.start();
+            thread2.start();
+        }
+    }
+}
