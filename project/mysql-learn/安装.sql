20ea8e2054c104841ab22a7069979e86fc2ce3bb
chenpiqian <chenpiqian@shenzhenpoly.com>
2019-06-21 18:21:10 +0800

    00

---
+++ project/mysql-learn/安装.sql
@@ -0,0 +1,76 @@
+
+wget http://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+yum -y install mysql57-community-release-el7-11.noarch.rpm
+
+查看一下安装效果
+yum repolist enabled | grep mysql.*
+
+yum install -y mysql-server
+
+systemctl start  mysql
+systemctl status mysql
+
+修改密码
+grep "password" /var/log/mysqld.log
+mysql -uroot -p
+改变validate_password_policy策略为0，仅校验密码长度，最小长度为8位
+set global validate_password_policy=0;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '密码'
+
+设置用户 root 可以在任意 IP 下被访问：
+grant all privileges on *.* to root@"%" identified by "密码";
+设置用户 root 可以在本地被访问：
+grant all privileges on *.* to root@"localhost" identified by "密码";
+
+刷新权限使之生效：
+flush privileges;
+
+
+查看 MySQL 的字符集：
+mysql> show variables like '%character%';
+
+查看指定的数据库中指定数据表的字符集，如查看 mysql 数据库中 servers 表的字符集：
+  show table status from mysql like '%servers%';
+查看指定数据库中指定表的全部列的字符集，如查看 mysql 数据库中 servers 表的全部的列的字符集：
+show full columns from servers;
+
+设置 MySQL 的字符集为 UTF-8：
+打开 /etc 目录下的 my.cnf 文件（此文件是 MySQL 的主配置文件）：
+/etc/my.cnf
+在 [mysqld] 前添加如下代码：
+[client]
+default-character-set=utf8
+在 [mysqld] 后添加如下代码：
+character_set_server=utf8
+再次查看字符集：
+mysql> show variables like '%character%';
++--------------------------+----------------------------+
+| Variable_name            | Value                      |
++--------------------------+----------------------------+
+| character_set_client    | utf8                      |
+| character_set_connection | utf8                      |
+| character_set_database  | utf8                      |
+| character_set_filesystem | binary                    |
+| character_set_results    | utf8                      |
+| character_set_server    | utf8                      |
+| character_set_system    | utf8                      |
+| character_sets_dir      | /usr/share/mysql/charsets/ |
++--------------------------+----------------------------+
+8 rows in set (0.01 sec)
+
+/var/lib/mysql 是存放数据库文件的目录；
+
+/var/log 目录下的 mysqld.log 文件记录 MySQL 的日志；
+
+忘记密码时，可用如下方法重置：
+# service mysqld stop
+# mysqld_safe --user=root --skip-grant-tables --skip-networking &
+# mysql -u root 
+mysql> use mysql;
+mysql> update user set password=password("new_password") where user="root"; 
+mysql> flush privileges;
+
+
+
+
+
--- project/mysql-learn/记录.sql
+++ project/mysql-learn/记录.sql
@@ -1,19 +1,110 @@
-遗留问题：
-    P8 重复读取bug
-    P9 死锁
-
-P9
-START TRANSACTION;
-UPDATE spring_cache SET tc = '11' WHERE num=11 AND NAME = 'n1';
-UPDATE spring_cache SET tc = '22' WHERE num=22 AND NAME = 'n2';
-COMMIT;
-
-START TRANSACTION;
-UPDATE spring_cache SET tc = '22' WHERE num=22 AND NAME = 'n2';
-UPDATE spring_cache SET tc = '11' WHERE num=11 AND NAME = 'n1';
-COMMIT;
+mysql 数据存储位置 /var/lib/mysql
+	tableName.frm  存储表结构
 
 
+Innodb存储数据的方式
+SHOW VARIABLES LIKE 'innodb_file_per_table';
+innodb_file_per_table	ON
+    表数据存储在独立表空间  tableName.idb  
+
+改变innodb_file_per_table为OFF  
+SET GLOBAL innodb_file_per_table='OFF';
+新建t2表
+/var/lib/mysql/learn/ 目录下只有t2.frm，没有t2.idb，数据存储在共享表空间/var/lib/mysql/ibdata1
+
+Redo Log、Undo Log 与事务关系
+Redo Log重做日志，存储已经提交的事务，实现事务持久性
+	重做事物缓冲区，每秒刷新到硬盘
+	innodb_log_buffer_size	16777216  #单位是字节，16M
+
+	重做日志文件
+	SHOW VARIABLES LIKE 'innodb_log_files_in_group';
+	innodb_log_files_in_group	2
+	有两个文件/var/lib/mysql/目录下的ib_logfile0、ib_logfile1
+
+
+Undo Log回滚日志，存储未提交的事务
+
+
+mysql读锁是共享锁，写锁是独占锁；读锁与读锁可以并发执行，写锁与任何锁都互斥
+执行
+begin
+UPDATE table_name SET field1=new-value1
+
+另一个标签页执行
+select * from table_name where field1=new-value1;
+仍然可以获取数据，似乎没有被写锁排斥，造成这样的原因是此时select语句是读取Undo Log中的数据
+
+
+
+# 表级锁
+LOCK TABLE t WRITE;
+# 在另一个标签页执行查询，查询被阻塞
+SELECT * FROM t;
+#解锁后查询被放行
+UNLOCK TABLES;
+
+
+mysql读取配置文件的顺序
+/etc/my.cnf   /etc/mysql/my.nf  /home/mysql/my.cnf   ~/.my.cnf
+
+设置全局参数
+set global 参数名=参数值
+set @@global.参数名:=参数值 (在sql语句中使用)
+set @@global.参数名=参数值
+有些参数即便设置为全局有效，但对于之前创建的mysql连接，任然使用旧的参数值，新建的连接才使用新参数值。例如wait_timeout、interactive_timeout
+
+会话参数
+set [session] 参数名=参数值
+set @@session.参数名:=参数值 (在sql语句中使用)
+set @@session.参数名=参数值
+
+
+为每个连接线程分配的缓冲
+sort_buffer_size   每个连接排序缓冲区的内存
+join_buffer_size   连接缓冲区，每连接查询一张表，申请一个
+read_buffer_size   myisam读缓冲区大小，要是4K的倍数
+read_rnd_buffer_size   查询索引缓冲区大小
+
+缓冲池内存
+Innodb_buffer_pool_size 官方建议为mysql服务器宿主机75%以上
+
+
+
+Innodb_log_file_size  单个事物日志的大小
+Innodb_log_files_in_group 事物日志文件个数，这个不需要改
+事物日志中大小 Innodb_log_file_size * Innodb_log_files_in_group
+
+事物日志不是每次提交事物都写入事物日志中，而是先写入事物日志缓冲区
+事物日志缓冲区 Innodb_log_buffer_size，一秒后写入事物日志文件
+
+
+
+Innodb_flush_method=O_DIRECT  避免Innodb、操作系统双缓存
+
+
+expire_logs_days  指定自动清理binlog的天数
+max_allowed_packet 控制Mysql可以接受的包的大小，可以设置大点
+
+read_only 禁止非super权限用户写权限，用户备库中
+skip_slave_start 禁用slave自动恢复
+sql_mode  only_fuul_group_by  group by 后要列出查询列中所有非聚合函数的列
+sync_binlog 默0是系统决定，主库要设置为1，每次事务完成后都写二进制日志文件，避免主库崩溃，有一部分二进制日志在缓冲中，导致主备不一致
+
+max_connections 最大连接数，默认100，太小，可以设置为2000
+
+
+基准测试 mysqlslap
+mysqlslap --password=密码 --concurrency=1,50,100 --iterations=3 --number-int-cols=5 --number-char-cols=5 --auto-generate-sql --auto-generate-sql-add-autoincrement --engine=innodb --number-of-queries=10 --create-schema=sbtest
+
+--only-print  打印sql，不运行测试
+mysqlslap --password=密码 --concurrency=1,50,100 --iterations=3 --number-int-cols=5 --number-char-cols=5 --auto-generate-sql --auto-generate-sql-add-autoincrement --engine=innodb --number-of-queries=10 --create-schema=sbtest --only-print
+
+
+
+基准测试sysbench
+curl -s https://packagecloud.io/install/repositories/akopytov/sysbench/script.rpm.sh | sudo bash
+sudo yum -y install sysbench
 
 
 
@@ -21,19 +112,13 @@ COMMIT;
 
 
 
-p609
-符号分割文件备份(备份中没有表结构)
-select * into outfile '输出文件必须是secure_file_priv的值/var/lib/mysql-files/book1.txt' fields terminated by ',' optionally enclosed by '"' lines terminated by '\n' from cpq.book;
 
-book1.txt存储结果：
-1,"12354","aaa","iii","ccc"
-2,"22vv","aqa222","ii22","cc21111"
 
-查看secure_file_priv
-show variables like '%secure%'
-secure_file_priv	/var/lib/mysql-files/
 
-还原备份
-load data infile '/var/lib/mysql-files/book1.txt' into table cpq.book fields terminated by ',' optionally enclosed by '"' lines terminated by '\n';
+
+
+
+
+
 
 
