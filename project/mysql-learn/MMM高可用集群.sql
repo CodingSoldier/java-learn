de05cd5458eeb1b347aa1bd8a7cac82d5de9fd33
chenpiqian <chenpiqian@shenzhenpoly.com>
2019-06-24 11:50:00 +0800

    00

--- project/mysql-learn/MMM高可用集群.sql
+++ project/mysql-learn/MMM高可用集群.sql
@@ -112,3 +112,57 @@ make install
 yum install mysql-mmm-agent.noarch -y
 
 
+索引使用sakila数据库演示
+
+B-tree索引的叶子节点存放数据主键
+mysql发生一次io，默认能读取16K，索引比数据行要小，所以读取一页能获取索引数量比数据行数多
+索引使用键值存放，排序的时候可以避免生成临时表
+B-tree索引有顺序，mysql查找索引是顺序io
+
+
+索引列上不能使用表达式或函数
+where to_days(out_date)  out_date的索引无效
+
+覆盖索引，索引中存放了select子句中要查询的值，explain的Extra行显示Using index则为使用了覆盖索引。比方说select的查询列只包含主键，主键值就在索引的叶子节点上
+
+
+#Extra结果是Using index condition;
+EXPLAIN SELECT * FROM rental WHERE rental_date='2005-05-09' ORDER BY inventory_id, customer_id;
+#Extra结果是Using index condition; Using filesort。
+#因为order by的排序和索引排序不一致，mysql无法利用索引完成排序操作的时候，MySQL Query Optimizer 不得不选择相应的排序算法来实现。
+EXPLAIN SELECT * FROM rental WHERE rental_date='2005-05-09' ORDER BY inventory_id DESC, customer_id;
+#Extra结果Using where; Using filesort。最左边的列使用范围查找，右边的列将无法使用索引
+EXPLAIN SELECT * FROM rental WHERE rental_date>'2005-05-09' ORDER BY inventory_id, customer_id;
+
+使用Hash索引，多加一列存储需要索引列的hash值，查询时候在where后加上hash列，需要索引的列作为条件；还需要加上索引列是因为不同值的hash值可能会重复
+
+
+Innodb使用行级锁，但只有在Innodb存储引擎层过滤掉不需要的行时才有效，如果储存引擎不能过滤行，则需要锁定所有行
+拷贝actor表
+use sakila; 
+begin;
+select * from actor_copy1 where last_name='WOOD' for update;
+
+另一个标签页
+begin;
+select * from actor_copy1 where last_name='Wills' for update;
+能更新成功
+
+所以连接回滚
+rollback;
+
+删除last_name索引，重复上面的操作，发现
+select * from actor_copy1 where last_name='Wills' for update;
+未能执行，由于Innodb未能通过索引筛选出last_name='WOOD'的行，所以全表锁定
+
+
+重复索引
+unique key也是一种索引，列建立了unique后就没有必要在此列建立其他索引了
+index(a), index(a,b)  重复
+
+
+
+
+
+
+
