70985f204912cb0c2210c1a569f6a6eb5f64617b
CodingSoldier <tfz9011@163.com>
2019-07-15 22:44:24 +0800

    00

---
+++ project/spring-boot-imooc/g-spring-webflux/src/main/java/com/example/cpq/gspringwebflux/WebFluxApplication.java
@@ -0,0 +1,54 @@
+package com.example.cpq.gspringwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+/**
+ * @auther: madison
+ * @date: 2018/10/22 20:33
+ * @description:
+ */
+
+@SpringBootApplication
+@RestController
+public class WebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxApplication.class);
+    }
+
+    //// 原始方式
+    //@Bean
+    //public RouterFunction<ServerResponse> routerFunction(){
+    //    return RouterFunctions.route(request -> {
+    //        URI uri = request.uri();
+    //        return "/hello-world".equals(uri.getPath());
+    //    }, request -> {
+    //        Mono<ServerResponse> mono = ServerResponse.status(HttpStatus.OK)
+    //                .body(Mono.just("hello, world"), String.class);
+    //        return mono;
+    //    });
+    //}
+
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction() {
+
+        return route(GET("/hello-world"), this::hellWorld);
+    }
+
+    public Mono<ServerResponse> hellWorld(ServerRequest serverRequest) {
+        return ServerResponse.status(HttpStatus.OK)
+                .body(Mono.just("Hello,World"), String.class);
+    }
+
+}
