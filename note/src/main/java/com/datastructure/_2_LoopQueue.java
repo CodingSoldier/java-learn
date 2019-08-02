a51d1edd908b9cf169fd78fd914c07fbc80cfbec
chenpiqian <chenpiqian@shenzhenpoly.com>
2019-08-02 15:42:14 +0800

    00

--- note/src/main/java/com/datastructure/_2_LoopQueue.java
+++ note/src/main/java/com/datastructure/_2_LoopQueue.java
@@ -13,61 +13,83 @@ public class _2_LoopQueue {
      * 循环队列的出队（弹出第一个元素）,不需要移动元素下标，均摊复杂度是 O(1)
      * 数组队列的出队（弹出第一个元素），需要移动所有元素下标，均摊复杂度是 O(n)
      */
+
+    // 基于循环数组实现队列
     static class LoopQueue<E>{
-        private E[] data;
-        private int front, tail;
-        private int size;
+        private E[] data;  // 数组
+        private int front, tail;  //队头、队尾
+        private int size;  //队列元素大小
 
         public LoopQueue(){
-            data = (E[]) new Object[11];
+            data = (E[]) new Object[11];  //初始化队列的数组
+            // 初始化时队头、队尾下标都为0，队列大小也是0
             front = 0;
             tail = 0;
             size = 0;
         }
 
+        // 队列容量是数组长度减1，因为要空一个元素，以便区别队列是否为空、队列是否满了需要扩容
         public int getCapacity(){
             return data.length -1;
         }
 
+        //队列为空
         public boolean isEmpty(){
             return front == tail;
         }
 
+        //队列大小
         public int getSize(){
             return size;
         }
 
+        //扩容、缩容
+        private void resize(int newCapacity){
+            // 数组长度要比容量多一个
+            E[] newData = (E[])new Object[newCapacity+1];
+            //将旧数据从队头开始，添加到新数组中
+            for (int i=0; i<size; i++){
+                newData[i] = data[(i+front) % data.length];
+            }
+            front = 0;  //添加完成，队头在新数组下标为0的位置
+            tail = size;  //添加完成，队列大小就是队尾的下标
+            data = newData;  //队列的数组data指向扩容后的数组
+        }
+
+        //入队
         public void enqueue(E e){
+            //如果队列满了，则扩容
             if ((tail + 1) % data.length == front){
                 resize(getCapacity() * 2);
             }
             data[tail] = e;
+            //队尾下标移动
             tail = (tail + 1) % data.length;
+            //队列大小加一
             size++;
         }
 
+        //出队
         public E dequeue(){
             if (isEmpty())
                 throw new RuntimeException("没数据了");
+            //队头数据存储到变量中
             E ret = data[front];
             data[front] = null;
+            //队头下标移动
             front = (front + 1) % data.length;
+            //队列大小减一
             size--;
+
+            //这是缩容，如果队列大小小于容量的1/4且容量的1/2不为0，队列缩容为原来的1/2
             if (size < getCapacity()/4 && getCapacity()/2 != 0){
                 resize(getCapacity()/2);
             }
+            //返回队头数据
             return ret;
         }
 
-        private void resize(int newCapacity){
-            E[] newData = (E[])new Object[newCapacity+1];
-            for (int i=0; i<size; i++){
-                newData[i] = data[(i+front) % data.length];
-            }
-            front = 0;
-            tail = size;
-            data = newData;
-        }
+
 
         @Override
         public String toString(){
@@ -91,7 +113,7 @@ public class _2_LoopQueue {
                 queue.enqueue(i);
                 System.out.println(queue);
 
-                if(i % 3 == 2){
+                if(i % 8 == 2){
                     queue.dequeue();
                     System.out.println(queue);
                 }
--- note/src/main/java/com/datastructure/_3_LinkListQueue.java
+++ note/src/main/java/com/datastructure/_3_LinkListQueue.java
@@ -7,11 +7,13 @@ package com.datastructure;
  */
 public class _3_LinkListQueue {
 
-    //使用链表实现一个单项队列
+    //使用链表实现一个队列
     public static class LinkListQueue<E>{
         // 链表节点
         private class Node{
+            // 当前节点值
             public E e;
+            // 当前节点的next属性指向下一个节点
             public Node next;
 
             public Node(E e, Node next){
@@ -33,9 +35,10 @@ public class _3_LinkListQueue {
             }
         }
 
-        private Node head, tail;
-        private int size;
+        private Node head, tail;  //队首、队尾
+        private int size;  //队列大小
 
+        // 初始化队列
         public LinkListQueue(){
             head = null;
             tail = null;
@@ -50,29 +53,39 @@ public class _3_LinkListQueue {
             return size == 0;
         }
 
+        // 入队
         public void enqueue(E e){
+            // tail == null  队列中没有元素
             if (tail == null){
                 tail = new Node(e);
                 head = tail;
             }else {
+                // 队列中已经有元素了，末尾元素的next指向一个新建的Node节点
                 tail.next = new Node(e);
+                // 队列类中维护的tail属性（队尾）也要指向新建的节点
                 tail = tail.next;
             }
-            size++;
+            size++;  // 队列大小加一
         }
 
+        //出队
         public E dequeue(){
             if (isEmpty())
-                throw new RuntimeException("下标问题");
+                throw new RuntimeException("队列空了亲");
 
+            //获取队首节点
             Node cur = head;
+            // 队列类中维护的head属性（队首）指向下一节点
             head = head.next;
+            // 删除出队节点的引用，以便java虚拟机回收内存空间
             cur.next = null;
 
-            if (head.next == null)
+            // 队首指向null，表明队列中没有元素了
+            if (head == null)
                 tail = null;
 
-            size--;
+            size--;   // 队列大小减一
+            // 返回队首节点值
             return cur.e;
         }
 
@@ -97,10 +110,14 @@ public class _3_LinkListQueue {
                 queue.enqueue(i);
                 System.out.println(queue);
 
-                if(i % 3 == 2){
+                if(i % 3 == 0){
                     queue.dequeue();
                     System.out.println(queue);
                 }
+            }
+            for(int i = 0 ; i < 7 ; i ++){
+                queue.dequeue();
+                System.out.println(queue);
             }
         }
 
