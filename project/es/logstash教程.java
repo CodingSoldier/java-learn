43718a1d8a9003ca6239f3b67b25fb6b6a81b41e
CodingSoldier <tfz9011@163.com>
2019-09-26 22:27:28 +0800

    00

--- project/es/logstash教程.java
+++ project/es/logstash教程.java
@@ -30,7 +30,9 @@
       183.162.52.7 - - [10/Nov/2016:00:01:02 +0800] "POST /api3/getadv HTTP/1.1" 200 813 "www.imooc.com" "-" cid=0&timestamp=1478707261865&uid=2871142&marking=androidbanner&secrect=a6e8e14701ffe9f6063934780d9e2e6d&token=f51e97d1cb1a9caac669ea8acc162b96 "mukewang/5.0.0 (Android 5.1.1; Xiaomi Redmi 3 Build/LMY47V),Network 2G/3G" "-" 10.100.134.244:80 200 0.027 0.027
   使用 http://grokdebug.herokuapp.com/ 获取日志的grok配置
 
-
+使用x-pack 
+  logstash.yml开启注释内容
+  xpack.management.elasticsearch.url: ["http://localhost:9200"]
 
 
         
