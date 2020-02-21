e927a21dc5c346ee1f5d61095fbeedaec23fd438
chenpiqian <chenpiqian@shenzhenpoly.com>
2020-02-21 18:29:45 +0800

    00

---
+++ project/gradle/gradle01/src/main/java/Test01.java
@@ -0,0 +1,17 @@
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.ArrayList;
+
+/**
+ * @author chenpiqian
+ * @date: 2020-02-21
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        //System.out.println("######11111111111#######");
+
+        System.out.println(CollectionUtils.isNotEmpty(new ArrayList()));
+
+
+    }
+}
