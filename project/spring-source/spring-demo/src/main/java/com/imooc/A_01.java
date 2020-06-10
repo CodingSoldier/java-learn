dab006f44dd9ef158c1cea74994c8fb419aae53f
chenpiqian <chenpiqian@shenzhenpoly.com>
2020-06-10 18:27:19 +0800

    00

--- project/spring-source/spring-demo/src/main/java/com/imooc/A_01.java
+++ project/spring-source/spring-demo/src/main/java/com/imooc/A_01.java
@@ -9,10 +9,20 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
  */
 public class A_01 {
 	public static void main(String[] args) {
+
+		/**
+		 * 1、build.gradle导入依赖 compile(project(":spring-context"))
+		 * 2、新增WelcomeService接口与实现类
+		 * 3、编写spring-config.xml，配置 <bean id="welcomeService" class="com.imooc.a_service.WelcomeServiceImpl"/>
+		 * 4、通过以下代码获取bean，并调用sayHello方法
+		 *
+		 * 到此，证明我们自己编译spring源码，使用自己编译的jar包成功
+		 */
 		System.out.println("############");
 		String xmlPath="D:\\spring-framework-5.2.0.RELEASE\\spring-demo\\src\\main\\resources\\spring-config.xml";
 		FileSystemXmlApplicationContext appContext = new FileSystemXmlApplicationContext(xmlPath);
 		WelcomeService welcomeService = appContext.getBean("welcomeService", WelcomeService.class);
 		welcomeService.sayHello("成功了");
+
 	}
 }
