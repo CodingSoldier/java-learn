8b1094b24f46e4b49eec3c2754187ae6aa3c184f
chenpiqian <chenpiqian@shenzhenpoly.com>
2020-07-22 18:25:41 +0800

    00

--- project/spring-source/spring-learn-01/src/main/java/com/learn/b_bean/B5_BeanDefinitionReader.java
+++ project/spring-source/spring-learn-01/src/main/java/com/learn/b_bean/B5_BeanDefinitionReader.java
@@ -14,10 +14,25 @@ public class B5_BeanDefinitionReader {
 	public static void main(String[] args) {
 		/**
 		 org.springframework.beans.factory.xml.XmlBeanDefinitionReader#loadBeanDefinitions(org.springframework.core.io.Resource)
+		 org.springframework.beans.factory.xml.XmlBeanDefinitionReader#registerBeanDefinitions(org.w3c.dom.Document, org.springframework.core.io.Resource)
+		 org.springframework.beans.factory.xml.DefaultBeanDefinitionDocumentReader#processBeanDefinition(org.w3c.dom.Element, org.springframework.beans.factory.xml.BeanDefinitionParserDelegate)
+
 		 打断点，运行代码调试
+
+		 往注册表注册一个新的BeanDefinition实例
+		 org.springframework.beans.factory.support.BeanDefinitionRegistry#registerBeanDefinition(java.lang.String, org.springframework.beans.factory.config.BeanDefinition)
+
+		 移除BeanDefinition
+		 org.springframework.beans.factory.support.BeanDefinitionRegistry#removeBeanDefinition(java.lang.String)
+
+		 BeanDefinition注册工具类
+		 org.springframework.beans.factory.support.BeanDefinitionReaderUtils#registerBeanDefinition(org.springframework.beans.factory.config.BeanDefinitionHolder, org.springframework.beans.factory.support.BeanDefinitionRegistry)
 		 */
 		String xmlPath="D:\\spring-framework-5.2.0.RELEASE\\spring-learn-01\\src\\main\\resources\\spring\\spring-config.xml";
 		FileSystemXmlApplicationContext applicationContext = new FileSystemXmlApplicationContext(xmlPath);
+		Object user1 = applicationContext.getBean("user1");
+		System.out.println(user1);
+
 
 	}
 }
