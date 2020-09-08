3367f6714802763d9cb369bfacbeacff8afc9fc2
chenpiqian <chenpiqian@shenzhenpoly.com>
2020-09-08 18:30:19 +0800

    00

---
+++ project/spring-boot-deep/d-spring-mvc/src/main/java/com/example/cpq/dspringmvc/controller/Test01.java
@@ -0,0 +1,14 @@
+package com.example.cpq.dspringmvc.controller;
+
+public class Test01 {
+
+   public static void main(String[] args) {
+        int i = 0;
+        System.out.println(i--);
+        System.out.println(i);
+
+        i = 0;
+        System.out.println(--i);
+        System.out.println(i);
+    }
+}
