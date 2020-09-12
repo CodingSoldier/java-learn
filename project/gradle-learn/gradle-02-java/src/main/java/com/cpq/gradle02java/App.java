6f71b3b528afa5b64c2b916f6c6320be2113829f
CodingSoldier <tfz9011@163.com>
2020-09-12 14:55:20 +0800

    00

--- project/gradle-learn/gradle-02-java/src/main/java/com/cpq/gradle02java/App.java
+++ project/gradle-learn/gradle-02-java/src/main/java/com/cpq/gradle02java/App.java
@@ -1,13 +1,17 @@
 package com.cpq.gradle02java;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(App.class);
         int i=0;
         Scanner scanner = new Scanner(System.in);
         while (++i > 0){
-            System.out.println(i + ". please input todo item name");
+            logger.info(i + ". 请输入: ");
             TodoItem todoItem = new TodoItem(scanner.nextLine());
             System.out.println(todoItem);
         }
