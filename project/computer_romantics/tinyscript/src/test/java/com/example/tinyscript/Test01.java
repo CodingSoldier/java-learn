04d8d7cc4de311df27bd04900d1dada0718c21cc
chenpiqian <chenpiqian@shenzhenpoly.com>
2020-09-18 17:40:59 +0800

    00

---
+++ project/computer_romantics/tinyscript/src/test/java/com/example/tinyscript/Test01.java
@@ -0,0 +1,15 @@
+package com.example.tinyscript;
+
+/**
+ * @author chenpiqian
+ * @date: 2020-09-10
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+        char c = 127;
+        c++;
+        System.out.println("### " + c);
+    }
+
+}
