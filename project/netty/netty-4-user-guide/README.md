04554b762328b6ee457a7f0431a163e907a9a55a
CodingSoldier <tfz9011@163.com>
2021-11-30 23:11:29 +0800

    00

---
+++ project/netty/netty-4-user-guide/README.md
@@ -0,0 +1,2 @@
+Netty 4.x User Guide《Netty 4.x 用户指南》
+https://waylau.com/netty-4-user-guide/Getting-Started/Getting-Started.html
---
+++ project/netty/netty-4-user-guide/src/main/java/org/cpq/A_DiscardServer.java
@@ -0,0 +1,53 @@
+package org.cpq;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class A_DiscardServer {
+
+    private int port;
+
+    public A_DiscardServer(int port){
+        this.port = port;
+    }
+
+    public void run() throws Exception{
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new A_DiscardServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            // 绑定端口，开始接受进来的连接
+            ChannelFuture f = b.bind(port).sync();
+
+            // 等待服务器 socket 关闭
+            // 在这个例子中，这不会发生，但是可以优雅的关闭你的服务器
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = args.length > 0 ? Integer.parseInt(args[0]) : 8080;
+        new A_DiscardServer(port).run();
+    }
+
+
+}
