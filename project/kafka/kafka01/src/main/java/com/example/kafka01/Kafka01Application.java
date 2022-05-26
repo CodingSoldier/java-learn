25d766e7efdc779a34b078c910d71d7f9514ef8c
CodingSoldier <tfz9011@163.com>
2022-05-26 21:54:44 +0800

    00

---
+++ project/kafka/kafka01/src/main/java/com/example/kafka01/Kafka01Application.java
@@ -0,0 +1,13 @@
+package com.example.kafka01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafka01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kafka01Application.class, args);
+    }
+
+}
---
+++ project/kafka/kafka01/src/test/java/com/example/kafka01/Kafka01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kafka01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
