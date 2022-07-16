2b1a6783a6882ccf1850dbf0a76d57ca25e14c14
CodingSoldier <tfz9011@163.com>
2022-07-16 18:10:02 +0800

    00

--- project/reactor/reactor0/src/main/java/com/example/reactor0/Reactor0Application.java
+++ project/reactor/reactor0/src/main/java/com/example/reactor0/Reactor0Application.java
@@ -3,11 +3,19 @@ package com.example.reactor0;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class Reactor0Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Reactor0Application.class, args);
+        List<Thread> collect = Thread.getAllStackTraces()
+                .keySet()
+                .stream()
+                .collect(Collectors.toList());
+        System.out.println(collect.toString());
     }
 
 }
---
+++ project/reactor/reactor0/src/main/java/com/example/reactor0/a07serversentevents/A07Controller.java
@@ -0,0 +1,39 @@
+package com.example.reactor0.a07serversentevents;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+@RestController
+public class A07Controller {
+
+    /**
+     * 使用浏览器访问
+     * @return
+     */
+    @GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamFlux() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> "Flux - " + LocalTime.now().toString());
+    }
+
+    /**
+     * 使用浏览器访问，Flux是单向通行，服务端向客户端推送消息
+     * @return
+     */
+    @GetMapping("/stream-sse")
+    public Flux<ServerSentEvent<String>> streamEvents() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> ServerSentEvent.<String>builder()
+                        .id(String.valueOf(sequence))
+                        .event("periodic-event")
+                        .data("SSE - " + LocalTime.now().toString())
+                        .build());
+    }
+
+}
