39e7d3bd7b7a3dbfcf06ffca4e48fc0a6c879459
陈丕迁 <chenpq05@vanke.com>
2023-11-14 17:37:48 +0800

    apollo

---
+++ project/apollo/apollo-01/src/main/java/com/example/apollo01/ValueController.java
@@ -0,0 +1,31 @@
+package com.example.apollo01;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chenpq05
+ * @since 2023/11/14 15:59
+ */
+@RestController
+public class ValueController {
+
+  @Value("${test.value01}")
+  private String value01;
+
+  @Value("${test9}")
+  private String test9;
+
+  @Value("${test.public1}")
+  private String testpublic1;
+
+  @GetMapping("/value01")
+  public String value01() {
+    System.out.println("#################value01="+value01);
+    System.out.println("#################test9="+test9);
+    System.out.println("#################testpublic1="+testpublic1);
+    return "返回结果："+value01;
+  }
+
+}
---
+++ project/apollo/apollo-01/src/test/java/com/example/apollo01/Apollo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apollo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apollo01ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
