180a0837f3eedb021f4154286d7daddd76a573bb
陈丕迁 <chenpq05@vanke.com>
2024-02-18 16:13:12 +0800

    kafaka

---
+++ project/kafka/kafka01/src/main/java/com/example/kafka01/SimpleGroupConsumer.java
@@ -0,0 +1,42 @@
+package com.example.kafka01;
+
+import java.util.Arrays;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+/**
+ * @author chenpq05
+ * @since 2024/2/2 14:00
+ */
+public class SimpleGroupConsumer {
+
+  public static void main(String[] args) throws Exception {
+    String topic = "first-p";
+    String group = "my-group";
+    Properties props = new Properties();
+    props.put("bootstrap.servers", "localhost:9092");
+    props.put("group.id", group);
+    props.put("enable.auto.commit", "true");
+    props.put("auto.commit.interval.ms", "1000");
+    props.put("session.timeout.ms", "30000");
+    props.put("key.deserializer",
+        "org.apache.kafka.common.serialization.StringDeserializer");
+    props.put("value.deserializer",
+        "org.apache.kafka.common.serialization.StringDeserializer");
+    KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
+    consumer.subscribe(Arrays.asList(topic));
+    System.out.println("Subscribed to topic " + topic);
+    int i = 0;
+
+    while (true) {
+      ConsumerRecords<String, String> records = consumer.poll(100);
+      for (ConsumerRecord<String, String> record : records)
+        System.out.printf("offset = %d, key = %s, value = %s\n",
+            record.offset(), record.key(), record.value());
+    }
+  }
+
+}
---
+++ project/kafka/kafka01/src/main/java/com/example/kafka01/SimpleProducer.java
@@ -0,0 +1,54 @@
+package com.example.kafka01;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+/**
+ * @author chenpq05
+ * @since 2024/2/1 16:31
+ */
+public class SimpleProducer {
+
+  public static void main(String[] args) {
+    // create instance for properties to access producer configs
+    Properties props = new Properties();
+
+    //Assign localhost id
+    props.put("bootstrap.servers", "localhost:9092");
+
+    //Set acknowledgements for producer requests.
+    props.put("acks", "all");
+
+    //If the request fails, the producer can automatically retry,
+    props.put("retries", 0);
+
+    //Specify buffer size in config
+    props.put("batch.size", 16384);
+
+    //Reduce the no of requests less than 0
+    props.put("linger.ms", 1);
+
+    //The buffer.memory controls the total amount of memory available to the producer for buffering.
+    props.put("buffer.memory", 33554432);
+
+    props.put("key.serializer",
+        "org.apache.kafka.common.serialization.StringSerializer");
+
+    props.put("value.serializer",
+        "org.apache.kafka.common.serialization.StringSerializer");
+
+    Producer<String, String> producer = new KafkaProducer
+        <String, String>(props);
+
+    for(int i = 0; i < 10; i++) {
+      producer.send(new ProducerRecord("first-p", Integer.toString(i), Integer.toString(i)));
+    }
+
+    producer.flush();
+    System.out.println("Message sent successfully");
+    producer.close();
+  }
+
+}
